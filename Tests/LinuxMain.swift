62591b56dbda3aa9d7dc30ffcbe003adb42173b4
Joannis Orlandos <joannis@orlandos.nl>
2017-07-07 08:36:50 +0200

    rename tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,6 @@
 import XCTest
-@testable import MongoKitten_ProviderTests
+@testable import MongoKittenProviderTests
 
 XCTMain([
-    testCase(MongoKitten_ProviderTests.allTests),
+    testCase(MongoKittenProviderTests.allTests),
 ])
