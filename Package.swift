dc488d5f3800b220747ab342f69502ffed0b7df9
Joannis Orlandos <joannis@orlandos.nl>
2017-09-27 12:12:00 +0200

    mongokitten swift 3 lock

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "MongoKittenProvider",
     dependencies: [
-        .Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 4),
+        .Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 4, minor: 0),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
     ]
 )
